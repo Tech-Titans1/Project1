d6a3cbf95c22065038e0e6cf4ca63a05d23a3d34
Sinelizwi <65957121+Sinelizwi-design@users.noreply.github.com>
2024-08-29 09:17:02 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
+// Select the icon element by its id
+const icon = document.getElementById('myIcon');
+
+// Define a function to show an alert when the icon is clicked
+function showAlert() {
+    alert('Added to cart!');
+}
+
+// Add a click event listener to the icon
+icon.addEventListener('click', showAlert);
 
